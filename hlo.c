9206f3a238fe237eb1e01019f4deedaa5a046c40
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-03-01 11:01:35 +0530

    add

---
+++ hlo.c
@@ -0,0 +1,10 @@
+ #include<stdio.h>
+
+int main(){
+	int num1 , num2 , mult , div , add , subst;
+	char op;
+	printf("enter no.1");
+	scanf("%d" , &num1);
+        printf("enter no.2");
+        scanf("&d" , &num2);
+	
